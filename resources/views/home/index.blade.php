3f094e2e70c09d2a508227dd554f01de8a5fbf59
Javier <ngn.vidal@gmail.com>
2018-11-23 17:51:39 -0300

    Mejora visualizacion cliente (incompleta)

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,26 +1,62 @@
 @extends('layouts.app')
 @section('content')
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
 
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="https://picsum.photos/1200/300/?random" alt="">
+        </div>
+        <div class="item">
+            <img src="https://picsum.photos/1200/300/?random" alt="">
+        </div>
+        <div class="item">
+            <img src="https://picsum.photos/1200/300/?random" alt="">
+        </div>
+    </div>
 
-<div class="container text-center">
-	<div id="products">
-		@foreach($productos as $producto)
-		<div class="product white-panel">
-			<div class="producto-titulo">
-				<h5>{{ $producto->titulo }}</h5><hr>
-			</div>
-			<img src="{{ $producto->img }}" width="200" height="200">
-			<div class="product-info panel">
-				<div><h4>Precio: ${{ $producto->precio }}</h4></div>
-				<p>
-					<a class="btn btn-warning" href="{{ route('cart-add', $producto->id) }}">Agregar</a>
-					<a class="btn btn-primary" href="{{ route('home.show',$producto->id) }}">Detalles</a>
-				</p>
-			</div>
-		</div>
-		@endforeach
+    <!-- Controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<div class="banner panel panel-default">
+	<div class="panel-body text-center">
+		<span class="glyphicon glyphicon-th"></span> NUESTROS PRODUCTOS <span class="glyphicon glyphicon-th"></span>
 	</div>
-	{!!$productos->render()!!}
+</div>
+<div class = "body-home">
+		<div class="container text-center">
+			<div id="products">
+				@foreach($productos as $producto)
+				<div class="product white-panel">
+					<div class="producto-titulo">
+						<h5>{{ $producto->titulo }}</h5><hr>
+					</div>
+					<img src="{{ $producto->img }}" width="200" height="200">
+					<div class="product-info panel">
+						<div><h4>Precio: ${{ $producto->precio }}</h4></div>
+						<p>
+							<a class="btn btn-warning" href="{{ route('cart-add', $producto->id) }}">Agregar</a>
+							<a class="btn btn-primary" href="{{ route('home.show',$producto->id) }}">Detalles</a>
+						</p>
+					</div>
+				</div>
+				@endforeach
+			</div>
+			{!!$productos->render()!!}
+		</div>
 </div>
 
 @endsection
